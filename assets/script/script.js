e2863677f38451c06257a3bb194657e395c2507f
ggulmom <youjinkim1988@gmail.com>
2023-05-12 00:47:36 -0700

    Display Current Weather

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,17 +4,17 @@ $(function () {
 
     $("#search-button").on("click", function() {
         var location = $("#location-input").val();
-        getWeather(location);
+        // getWeather(location);
+        getWeather("San Diego");
         
-
-        // console.log (location);
     } );
 
 });
 
 async function getWeather(location) {
-    const geocoding_url = "http://api.openweathermap.org/geo/1.0/direct?";
-    const weather_url = "http://api.openweathermap.org/data/2.5/forecast?";
+    const geocoding_url = "https://api.openweathermap.org/geo/1.0/direct?";
+    const forecast_url = "https://api.openweathermap.org/data/2.5/forecast?";
+    const weather_url = "https://api.openweathermap.org/data/2.5/weather?";
 
     let requestUrl = geocoding_url.concat(`q=${location}&limit=1&appid=${api_key}`);
 
@@ -26,29 +26,53 @@ async function getWeather(location) {
             
         let lat = response[0].lat;
         let lon = response[0].lon;
-        // console.log(lat,lon);
 
-        requestUrl = weather_url.concat(`lat=${lat}&lon=${lon}&appid=${api_key}`);
-        return requestUrl;
-    }).then(function(requestUrl) {
-        if(requestUrl != undefined)
-        {
-            console.log('ok');
-            $.ajax({
-                url: requestUrl,
-                method: "GET",
-            }).then(function(response) {
-                processWeatherData(response);
-            });
-        }
-        // console.log(requestUrl);
+        let weather_fetch_url = weather_url.concat(`lat=${lat}&lon=${lon}&appid=${api_key}&units=imperial`);
+        let forecast_fetch_url = forecast_url.concat(`lat=${lat}&lon=${lon}&appid=${api_key}&units=imperial`);
+
+        weatherFetch(weather_fetch_url);
+        // forecastFetch(forecast_fetch_url);
+
     });
-
-    // console.log(api_call);
 }
-// console.log ("JS loaded");
 
-function processWeatherData(data) {
-    console.log(data);
-    console.log(data.list);
+
+function weatherFetch(url) {
+    $.ajax({
+        url: url,
+        method: "GET",
+    }).then(function(response) {
+        updateWeather(response);
+    });
+};
+
+function updateWeather(item) {
+    console.log(item);
+
+    let dt = new Date(item.dt * 1000);
+    let yyyy = dt.getFullYear();
+    let mm = dt.getMonth() + 1;
+    let dd = dt.getDate();
+    let hh = dt.getHours();
+    let icon = item.weather[0].icon;
+    let icon_url = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    let temperature = item.main.temp;
+    let humidity = item.main.humidity;
+    let wind = item.wind.speed;
+
+    let dateStr = `${mm}/${dd}/${yyyy}`;
+    let city = item.name;
+
+    $("#weather-container div #city").text(`${city} (${dateStr})`);
+    $("#weather-container #icon").html(`<img src="${icon_url}"></img`);
+    $("#weather-container div #temperature").text(`Temp: ${temperature} °F`);
+    $("#weather-container div #wind").text(`Wind: ${wind} MPH`);
+    $("#weather-container div #humidity").text(`Humidity: ${humidity} %`);
+
+
+    console.log(dateStr, temperature, humidity, wind);
+    
 }
+    // let dt = new Date(data.list[0].dt);
+    // // let date_str = $.datepicker.formatDate('yymmdd', test);
+    // console.log(test);
